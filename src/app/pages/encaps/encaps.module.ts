227d5bfd9d1acec278916d14c99a0260e0f9aaf8
Reshma216 <reshmashaikbujji@gmail.com>
2020-11-30 01:13:17 -0800

    create encaps

--- src/app/pages/encaps/encaps.module.ts
+++ src/app/pages/encaps/encaps.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { EncapsRoutingModule } from './encaps-routing.module';
 import { EncapsComponent } from './encaps/encaps.component';
+import { CreateEncapsComponent } from './create-encaps/create-encaps.component';
 
 
 @NgModule({
-  declarations: [EncapsComponent],
+  declarations: [EncapsComponent, CreateEncapsComponent],
   imports: [
     CommonModule,
     EncapsRoutingModule
